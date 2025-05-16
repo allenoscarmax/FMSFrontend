0e8f4f85a80b77a6c68495187ef2c26ecbc14794
GE_EXPERTBOOK\shuai <shuai0406@gmail.com>
2025-05-16 17:01:17 +0800

    fixs/fixs-0516

---
+++ FMSFrontend/Models/Robot.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Media;
+
+namespace FMSFrontend.Models
+{
+    public partial class Robot : ObservableObject
+    {
+        [ObservableProperty]
+        private string name;
+
+        [ObservableProperty]
+        private string currentLocation;
+
+        [ObservableProperty]
+        private string currentAction;
+
+        [ObservableProperty]
+        private string nextAction;
+
+        [ObservableProperty]
+        private Brush statusRed;
+
+        [ObservableProperty]
+        private Brush statusYellow;
+
+        [ObservableProperty]
+        private Brush statusGreen;
+    }
+}
