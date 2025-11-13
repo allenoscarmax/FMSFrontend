583d00ce9d985a9c27ff1db39611306a5e272566
Allen Lai <allenoscarmax@gmail.com>
2025-11-13 09:29:26 +0800

    暫存

---
+++ FMSFrontend/Models/TimelineItemModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ControlzEx.Standard;
+using FMSFrontend.Controls;
+using FMSFrontend.Features.Dtos;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq; // ← for PageList
+using System.Windows.Media;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+namespace FMSFrontend.Models
+{
+    public class TimelineItemModel
+    {
+        public string Text { get; set; } = "";
+        public DateTime Time { get; set; }
+        public string Status { get; set; } = "";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
